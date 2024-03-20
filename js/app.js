86e9995a7c8f96deb2b8a3a9884b9a6e3d3c030d
Alleph Nogueira <126208329+AllephNogueira@users.noreply.github.com>
2024-03-20 12:08:01 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,61 @@
+carrinhoLimpo();
+let ProdutoTotal = 0;
+
+function adicionar(){
+    // Recuperar produtos
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1]; // Quero oque esta depois do R$
+    let quantidade = document.getElementById('quantidade').value;
+    if (quantidade < 1){
+        alert('Voce deve adicionar a quantidade de itens');
+        return;
+    }
+
+    produtosNoCarrinho(quantidade, nomeProduto, valorUnitario)
+      
+}
+
+
+
+function produtosNoCarrinho(quantidade, nomeProduto, valorUnitario){
+
+   
+    let valorTotal =  valorUnitario * quantidade; // PrecoUnitario * quantidade
+
+
+
+    // Adicionando produtos no carrinho com quantidade preço, nome e valorUnitario
+    let meuCarrinho = document.getElementById('lista-produtos');
+    meuCarrinho.innerHTML +=
+     // Aqui estou dizendo que meu carrinho vai incrementar.
+     `<section class="carrinho__produtos__produto">
+    <span class="texto-azul"></span> ${nomeProduto} <span class="texto-azul">${quantidade} x R$${valorUnitario} = ${valorTotal}</span><br>`;
+
+
+    ProdutoTotal = ProdutoTotal + valorTotal;
+    let precoFinal = document.getElementById('valor-total')
+    precoFinal.textContent = (ProdutoTotal) // Valor total dos produtos.
+
+}
+
+
+
+// Funçoes para remover os itens do carrinho.
+function carrinhoLimpo(){
+    let meuCarrinho = document.getElementById('lista-produtos');
+    meuCarrinho.innerHTML =  `<section class="carrinho__produtos__produto">
+    <span class="texto-azul"> </span>   <span class="texto-azul"> </span><br>`;
+
+
+    let precoFinal = document.getElementById('valor-total')
+    precoFinal.textContent = '0';
+}
+
+function limpar(){
+    carrinhoLimpo()
+    if(confirm('Remover os itens do seu carrinho?')){
+        alert('Voce removeu todos os itens do seu carrinho!')
+    }
+    
+}
